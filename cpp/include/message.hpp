4a7d1d2cbb53f249d87a7ad297f6c00045c94733
cuikai2021 <kaic@icbench.com>
2022-07-01 17:18:08 +0800

    update cpp agent

--- cpp/include/message.hpp
+++ cpp/include/message.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <fmt/core.h>
 #include "libmsgagent.h"
 
 namespace message {
@@ -28,11 +29,7 @@ namespace message {
     template<typename... Args>
     inline void sendMsg(level_enum level, const std::string &msgTemplate, const Args &... args) {
 
-        int size_s = std::snprintf(nullptr, 0, msgTemplate.c_str(), args ...) + 1; // Extra space for '\0'
-        auto size = static_cast<size_t>( size_s );
-        std::unique_ptr<char[]> buf(new char[size]);
-        std::snprintf(buf.get(), size, msgTemplate.c_str(), args ...);
-        std::string msg = std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+        std::string msg = fmt::format(msgTemplate, args ...);
 
         GoString msgTemplateGo = {msgTemplate.c_str(), static_cast<ptrdiff_t>(msgTemplate.size())};
         GoString msgGo = {msg.c_str(), static_cast<ptrdiff_t>(msg.size())};
