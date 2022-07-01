740347a0d5ffcd5a8143e728051154e50af2bca7
cuikai2021 <kaic@icbench.com>
2022-07-01 15:36:32 +0800

    cpp agent by cgo

--- cpp/include/message.hpp
+++ cpp/include/message.hpp
@@ -11,25 +11,31 @@ namespace message {
     };
 
     template<typename... Args>
-    void info(const std::string &message, const Args &... args) {
+    inline void info(const std::string &message, const Args &... args) {
         sendMsg(level_enum::INFO, message, args...);
     }
 
     template<typename... Args>
-    void warn(const std::string &message, const Args &... args) {
+    inline void warn(const std::string &message, const Args &... args) {
         sendMsg(level_enum::WARN, message, args...);
     }
 
     template<typename... Args>
-    void error(const std::string &message, const Args &... args) {
+    inline void error(const std::string &message, const Args &... args) {
         sendMsg(level_enum::ERROR, message, args...);
     }
 
     template<typename... Args>
-    void sendMsg(level_enum level, const std::string &message, const Args &... args);
+    inline void sendMsg(level_enum level, const std::string &msgTemplate, const Args &... args) {
 
-    template<typename ... Args>
-    std::string string_format(const std::string &format, const Args &... args);
+        int size_s = std::snprintf(nullptr, 0, msgTemplate.c_str(), args ...) + 1; // Extra space for '\0'
+        auto size = static_cast<size_t>( size_s );
+        std::unique_ptr<char[]> buf(new char[size]);
+        std::snprintf(buf.get(), size, msgTemplate.c_str(), args ...);
+        std::string msg = std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 
-    GoString buildGoString(const char *p, size_t n);
+        GoString msgTemplateGo = {msgTemplate.c_str(), static_cast<ptrdiff_t>(msgTemplate.size())};
+        GoString msgGo = {msg.c_str(), static_cast<ptrdiff_t>(msg.size())};
+        SendMessage(level, msgTemplateGo, msgGo);
+    }
 }
